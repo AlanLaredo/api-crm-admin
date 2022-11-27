181d984bbd01b84a0390c83f66ffabf8bacba8e8
Santiago Laredo RYZEN <santiagoalan1@gmail.com>
2022-11-27 13:37:20 -0600

    Se agrega configuración graphQl.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { ConfigModule } from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql'
+import { Module } from '@nestjs/common'
 
-import { ENVIRONMENTS, JOI_VALIDATION_SCHEMA } from '../environments'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import config from '../environments/config'
+import { ENVIRONMENTS, JOI_VALIDATION_SCHEMA } from '../environments'
 import { MODULES } from '../modules'
 
 @Module({
@@ -14,6 +16,13 @@ import { MODULES } from '../modules'
       load: [config],
       isGlobal: true,
       validationSchema: JOI_VALIDATION_SCHEMA
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // TODO: check this property @Alan installSubscriptionHandlers: true,
+      autoSchemaFile: 'admin-schemas.gql',
+      playground: true,
+      path: '/admin'
     }),
     ...MODULES
   ],
--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+
 // import { AuthModule } from 'src/auth/auth.module'
 import { DatabaseModule } from '../../database/database.module'
 
