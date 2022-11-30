d51fa1083dc00e8d6961a339ad3023c8c5ec2bca
Santiago Laredo RYZEN <santiagoalan1@gmail.com>
2022-11-30 15:35:53 -0600

    Process function model update.

--- src/database/mongoose/models/process/process-function.model.ts
+++ src/database/mongoose/models/process/process-function.model.ts
@@ -1,4 +1,5 @@
 import { ModelDefinition } from '@nestjs/mongoose'
+
 import { ProcessFunctionEntity, ProcessFunctionSchema } from 'src/entities/process'
 
 export const ProcessFunctionModel: ModelDefinition = {
