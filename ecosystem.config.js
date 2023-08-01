e625dad701ffd95a07a4e67efbc3b5c4faf35b5d
Santiago Laredo <slaredo@ragasa.com.mx>
2023-08-01 12:02:40 -0600

    Mod prod

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,19 +7,20 @@ module.exports = {
       JWT_SECRET: 'secret',
       JWT_EXPIRES_IN: '10d',
       SYSTEM_PORT: 3001,
-      MONGO_INITDB_ROOT_USERNAME: 'root',
-      MONGO_INITDB_ROOT_PASSWORD: 'root',
+      MONGO_INITDB_ROOT_USERNAME: 'm0ng0db',
+      MONGO_INITDB_ROOT_PASSWORD: 'm0ng0dbpr3pr0d',
       MONGO_AUTH_DATABASE: 'admin',
       MONGO_DEFAULT_DATABASE: 'jarboss_centercomm',
       MONGO_CONNECTION: 'mongodb',
       MONGO_PORT: 27017,
-      MONGO_HOST: 'localhost',
+      MONGO_HOST: '52.117.21.213',
       MONGO_HOST_REPLICATE: '',
       MONGO_PATH_TO_CA_CERT: '',
       MAILER_DIR_TEMPLATE: './src/mail/templates/',
       TRANSPORT_CONFIGURATION: 'smtps://santiagoalan1@gmail.com:cotpmnxdtviuxmof@smtp.gmail.com',
       MAILDEV_INCOMING_USER: 'santiagoalan1@gmail.com',
-      MAILDEV_INCOMING_PASS: 'cotpmnxdtviuxmof'
+      MAILDEV_INCOMING_PASS: 'cotpmnxdtviuxmof',
+      SYSTEM_BILLING_PATH: './'
     }
   }]
 }
